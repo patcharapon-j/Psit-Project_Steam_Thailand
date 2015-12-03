ff1abc4e3353534b5b69fe858ee43e2562f131b1
vampirefeem <vampirefeem@gmail.com>
2015-12-03 13:30:19 +0700

    Successfully pull data from txt and put into list & delete new line from raw data

--- process.py
+++ process.py
@@ -2,7 +2,18 @@
 import statistics
 def reader():
 	"""read data from txt file"""
+	data = []
 	f = open("Raw Data.txt", "r")
-	print(f.read())
+	head = None
+	temp = []
+	for run in f.read().split("\n"):
+		if "###" in run:
+			data.append([head, temp])
+			temp = []
+			head = run[4:-4]
+		else:
+			temp.append(int(run.split()[3]))
+	data.append([head, temp])
+	data = data[1:]
 
 reader()
