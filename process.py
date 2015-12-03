95ee4a6a181944692e1c20c267316c8e663a3981
Sanhaphat Srijumpa <fluke031@hotmail.com>
2015-12-03 14:34:42 +0700

    Tabs to Space
    eiei gum

--- process.py
+++ process.py
@@ -1,36 +1,36 @@
 """Process Rawdata and compute statistics values"""
 import statistics
 def reader():
-	"""read data from txt file"""
-	data = []
-	f = open("Raw Data.txt", "r")
-	head = None
-	temp = []
-	for run in f.read().split("\n"):
-		if "###" in run:
-			data.append([head, temp])
-			temp = []
-			head = run[4:-4]
-		else:
-			temp.append(int(run.split()[3]))
-	data.append([head, temp])
-	data = data[1:]
-	return data
+    """read data from txt file"""
+    data = []
+    f = open("Raw Data.txt", "r")
+    head = None
+    temp = []
+    for run in f.read().split("\n"):
+        if "###" in run:
+            data.append([head, temp])
+            temp = []
+            head = run[4:-4]
+        else:
+            temp.append(int(run.split()[3]))
+    data.append([head, temp])
+    data = data[1:]
+    return data
 
 def avgerage(data):
-	"""return list contain avg of all data"""
-	out = []
-	for run in data:
-		out.append([run[0], statistics.mean(run[1])])
-	return out
+    """return list contain avg of all data"""
+    out = []
+    for run in data:
+        out.append([run[0], statistics.mean(run[1])])
+        return out
 
 def normalizer(data):
-	"""return normalized data from 0 to 1"""
-	pass
+    """return normalized data from 0 to 1"""
+    pass
 
 def main():
-	"""handle main"""
-	data = reader()
-	avg_data = avgerage(data)
+    """handle main"""
+    data = reader()
+    avg_data = avgerage(data)
 
 main()
