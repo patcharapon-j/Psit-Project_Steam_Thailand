237f3d6b084324e796309aa17bc86f2078750414
vampirefeem <vampirefeem@gmail.com>
2015-12-03 13:38:55 +0700

    complete aveager function

--- process.py
+++ process.py
@@ -15,5 +15,18 @@ def reader():
 			temp.append(int(run.split()[3]))
 	data.append([head, temp])
 	data = data[1:]
+	return data
 
-reader()
+def avgerage(data):
+	"""return list contain avg of all data"""
+	out = []
+	for run in data:
+		out.append([run[0], statistics.mean(run[1])])
+	return out
+
+def main():
+	"""handle main"""
+	data = reader()
+	avgerage(data)
+
+main()
