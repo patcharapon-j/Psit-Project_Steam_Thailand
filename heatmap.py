46eedb610c63f1ad69981e503b983ce1833aa007
vampirefeem <vampirefeem@gmail.com>
2015-12-09 20:01:34 +0700

    prepare data for plot

--- heatmap.py
+++ heatmap.py
@@ -3,5 +3,19 @@ import process as pr
 import numpy as np
 from bokeh.plotting import *
 from bokeh.models import HoverTool
-data['games'] = [pr.reader()[x][0] for x in range(len(pr.reader()))]
-games = list(data['games'])
+data = pr.reader()
+games = [data[x][0] for x in range(len(data))]
+data2 = {}
+for run in data:
+    data2[run[0]] = run[1]
+h_game = []
+h_day = []
+h_value = []
+H_color = []
+for run in games:
+    for run2 in range(1, 61):
+        h_game.append(run)
+        h_day.append(run2)
+        h_value.append(data2[run][run2-1])
+        h_color = "#ddb7b1"
+
