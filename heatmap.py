69077db949f6aa1dbdb79b8e775e331bd6c0f445
vampirefeem <vampirefeem@gmail.com>
2015-12-09 19:51:28 +0700

    setup

--- heatmap.py
+++ heatmap.py
@@ -1,3 +1,7 @@
 """Create Heatmap"""
 import process as pr
-print(pr.reader())
+import numpy as np
+from bokeh.plotting import *
+from bokeh.models import HoverTool
+data['games'] = [pr.reader()[x][0] for x in range(len(pr.reader()))]
+games = list(data['games'])
