f3a0d5f23500add45b6997a46d16de3715dac159
Sanhaphat Srijumpa <fluke031@hotmail.com>
2015-12-09 22:06:34 +0700

    Completed Histrogram

--- Histogram.py
+++ Histogram.py
@@ -2,3 +2,15 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 from process import reader
 py.sign_in("IMFluky", "80ck3k2e4n")
+
+data = [
+    go.Bar(
+        x=[i for i in range(1, 60)],
+        y=reader()[0][1]
+    )
+]
+layout = go.Layout(
+    title=reader()[0][0],
+)
+fig = go.Figure(data=data, layout=layout)
+url = py.plot(fig, filename=reader()[0][0])
