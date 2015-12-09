381a751e8cdcf11b0f7a4c76023228f2936a2491
vampirefeem <vampirefeem@gmail.com>
2015-12-09 20:41:33 +0700

    bare testing

--- heatmap.py
+++ heatmap.py
@@ -3,13 +3,18 @@ import process as pr
 import plotly.plotly as py
 import plotly.graph_objs as go
 data2 = pr.reader()
-games = [data2[0][x] for x in range(len(data2))]
+games = [data2[x][0] for x in range(len(data2))]
+data2 = [run[1] for run in data2]
+data3 = []
+for run in range(len(data2[0])):
+    temp = []
+    for run2 in range(len(data2)):
+        temp.append(data2[run2][run])
+    data3.append(temp)
 data = [
     go.Heatmap(
-        z=[run[1:] for run in data2],
-        x=[games],
-        y=[str(x) for x in range(1, 61)]
+        z=data3
     )
 ]
-plot_url = py.plot(data, filename='labelled-heatmap')
-plot_url = py.plot(data, filename='basic-heatmap')
+plot_url = py.plot(data, filename='Steam-heatmap')
+
