c84605b1c67d3d56886631ea69f1d6a8e8670902
Chiibi <Chiibi_jame@hotmail.com>
2015-12-09 22:19:25 +0700

    Finished Table

--- Table_data.py
+++ Table_data.py
@@ -2,10 +2,10 @@ import plotly.plotly as pty
 import process as pr
 import statistics as stat
 from plotly.tools import FigureFactory
-
+pty.sign_in("Chiibi", "lm7ftxursd")
 data = pr.reader()
-data_avg = pr.avgerage()
-data_matrix = [['Games', 'Average', 'Ceiling', 'Floor', 'SD'],]
+
+data_matrix = [['Games', 'Average', 'Ceiling', 'Floor', 'SD']] + [[i[0], stat.mean(i[1]), min(i[1]), max(i[1]), stat.stdev(i[1])] for i in data]
 
 table = FigureFactory.create_table(data_matrix, index=True)
-pty.iplot(table, filename='index_table')
+url = pty.plot(table, filename='data_table')
