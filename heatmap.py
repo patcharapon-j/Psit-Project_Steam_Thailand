aaee2ea12fe865de248100d2fb346372238fcc69
vampirefeem <vampirefeem@gmail.com>
2015-12-09 20:07:48 +0700

    initalize plot

--- heatmap.py
+++ heatmap.py
@@ -11,11 +11,20 @@ for run in data:
 h_game = []
 h_day = []
 h_value = []
-H_color = []
+h_color = []
 for run in games:
     for run2 in range(1, 61):
         h_game.append(run)
         h_day.append(run2)
         h_value.append(data2[run][run2-1])
         h_color = "#ddb7b1"
+source = ColumnDataSource(
+    data=dict(games=h_game, day=h_game, color=h_color, value=h_value)
+)
 
+output_file = ("playtimeheatmep.html")
+TOOLS = "resize,hover,save,pan,box_zoom,wheel_zoom"
+p = figure(title="Thailand Steam Playtime 2015",
+    x_range=games, y_range=[str(x) for x in range(60, 0, -1)],
+    x_axis_location="above", plot_width=900, plot_height=400,
+    toolbar_location="left", tools=TOOLS)
