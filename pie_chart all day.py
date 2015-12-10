a389f4dcfced844b7283a0de7831c2e26cf905b9
Sanhaphat Srijumpa <fluke031@hotmail.com>
2015-12-10 13:19:14 +0700

    Completed pie_chart all day.py

--- pie_chart all day.py
+++ pie_chart all day.py
@@ -3,3 +3,12 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 from process import request_day
 py.sign_in("IMfluky", "80ck3k2e4n")
+
+fig = {
+    "data": [{"labels": [request_day(1)[i][0] for i in range(len(request_day(1)))],
+              "values": [request_day(1)[i][1] for i in range(len(request_day(1)))],
+              "type": "pie"}],
+    "layout": {"title": "Steam play time 60 day"}
+}
+
+url = py.plot(fig, filename="Test Pie Chart")
