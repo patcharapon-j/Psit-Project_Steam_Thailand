3f9bc3883fdedb93e926ac6e6aa184920615a33c
Boss'sr ShadowZ <spectreb.boss@gmail.com>
2015-12-10 12:44:46 +0700

    update
    add trace0
    add graphs
    remove print game list

--- areachart.py
+++ areachart.py
@@ -4,4 +4,30 @@ import process as information
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-print(information.reader()[0][0])
+
+
+#add data
+txt = "Day"
+axis_x = [txt+str(num+1) for num in range(60)] #add axis_x
+axis_y = [[information.reader()[num][1][i] for i in range(60)] for num in range(28)]
+### axis_y = []
+### for num in range(28):
+### 	axis_y.append([information.reader()[num][1][i] for i in range(60)])
+
+#trace data
+trace0 = go.Scatter(
+	x = axis_x,
+	y = axis_y[0],
+	text=str(axis_y[0]),
+    hoverinfo='x+text',
+    mode='lines',
+    line=dict(
+        width=0.5
+    ),
+    fill='tonexty'
+)
+
+data = [trace0]
+
+fig = go.Figure(data=data)
+plot_url=py.plot(fig)
