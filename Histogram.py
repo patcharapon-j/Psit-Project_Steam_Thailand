b7a73e1422158601c4f0e2f9ead656081344715b
Sanhaphat Srijumpa <fluke031@hotmail.com>
2015-12-10 11:53:05 +0700

    Completed Histogram for All Game Data

--- Histogram.py
+++ Histogram.py
@@ -3,14 +3,15 @@ import plotly.graph_objs as go
 from process import reader
 py.sign_in("IMFluky", "80ck3k2e4n")
 
-data = [
-    go.Bar(
-        x=[i for i in range(1, 60)],
-        y=reader()[0][1]
+for x in range(len(reader())):
+    data = [
+        go.Bar(
+            x=[i for i in range(1, 60)],
+            y=reader()[x][1]
+        )
+    ]
+    layout = go.Layout(
+        title=reader()[x][0],
     )
-]
-layout = go.Layout(
-    title=reader()[0][0],
-)
-fig = go.Figure(data=data, layout=layout)
-url = py.plot(fig, filename=reader()[0][0])
+    fig = go.Figure(data=data, layout=layout)
+    url = py.plot(fig, filename=reader()[x][0])
