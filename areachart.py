dc3da72483808c5672d4c01ae21a00420d515de7
Boss'sr ShadowZ <spectreb.boss@gmail.com>
2015-12-10 14:09:29 +0700

    update
    complete graphs
    bugs : list number

--- areachart.py
+++ areachart.py
@@ -10,18 +10,22 @@ import plotly.graph_objs as go
 txt = "Day"
 axis_x = [txt+str(num+1) for num in range(60)] #add axis_x
 axis_y = [[information.reader()[num][1][i] for i in range(60)] for num in range(28)]
-### axis_y = []
-### for num in range(28):
-### 	axis_y.append([information.reader()[num][1][i] for i in range(60)])
+
+#stack data
+y_stack = [1 for _ in range(28)]
+y_stack[0] = axis_y[0]
+for i in range(28):
+	if i > 0:
+		y_stack[i] = [y0+y1 for y0, y1 in zip(axis_y[i], y_stack[i-1])]
 
 #trace data
 trace = [None for _ in range(28)]
-
 for num in range(28):
 	trace[num] = go.Scatter(
 		x = axis_x,
-		y = axis_y[num],
-		hoverinfo = 'name+y+x', #show information when mouse is over
+		y = y_stack[num],
+		text = axis_y[num],
+		hoverinfo = 'name+x', #show information when mouse is over
 		name = information.reader()[num][0],
 	    mode ='lines',
 	    line = dict(
