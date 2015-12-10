fe3727b6883d1b1d914c354b1b8d81191b652cf1
Sanhaphat Srijumpa <fluke031@hotmail.com>
2015-12-10 13:44:33 +0700

    Completed pie_chart all day.py for 60 day and change API-Key

--- pie_chart all day.py
+++ pie_chart all day.py
@@ -2,13 +2,14 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 from process import request_day
-py.sign_in("IMfluky", "80ck3k2e4n")
+py.sign_in("IMfluky", "tq0lke37dz")
 
-fig = {
-    "data": [{"labels": [request_day(1)[i][0] for i in range(len(request_day(1)))],
-              "values": [request_day(1)[i][1] for i in range(len(request_day(1)))],
-              "type": "pie"}],
-    "layout": {"title": "Steam play time 60 day"}
-}
+for day in range(1, 61):
+    fig = {
+        "data": [{"labels": [request_day(day)[i][0] for i in range(len(request_day(day)))],
+                  "values": [request_day(day)[i][1] for i in range(len(request_day(day)))],
+                  "type": "pie"}],
+        "layout": {"title": "Steam Play Time Day "+str(day)}
+    }
 
-url = py.plot(fig, filename="Test Pie Chart")
+    url = py.plot(fig, filename="Play Time Pie Chart Day"+str(day))
