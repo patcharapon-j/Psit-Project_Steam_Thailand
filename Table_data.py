cacd33390ee6ea07bf68eaa12c485bd8a64dc048
Chiibi <Chiibi_jame@hotmail.com>
2015-12-10 11:46:11 +0700

    Change Display
    Now table can show number with commas. And limiting float point.

--- Table_data.py
+++ Table_data.py
@@ -5,7 +5,7 @@ from plotly.tools import FigureFactory
 pty.sign_in("Chiibi", "lm7ftxursd")
 data = pr.reader()
 
-data_matrix = [['Games', 'Average', 'Ceiling', 'Floor', 'SD']] + [[i[0], stat.mean(i[1]), max(i[1]), min(i[1]), stat.stdev(i[1])] for i in data]
+data_matrix = [['Games', 'Average', 'Ceiling', 'Floor', 'SD']] + [[i[0], '{0:,.2f}'.format(stat.mean(i[1])), '{0:,d}'.format(max(i[1])), '{0:,d}'.format(min(i[1])), '{0:,.2f}'.format(stat.stdev(i[1]))] for i in data]
 
 table = FigureFactory.create_table(data_matrix, index=True)
 url = pty.plot(table, filename='data_table')
